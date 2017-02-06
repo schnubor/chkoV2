f7f0c46ab81e1d26317c59d5a0d220c697ef7a78
Christian <christian@styla.com>
2017-02-06 13:46:55 +0100

    updated links

--- projects/fortherecord/index.php
+++ projects/fortherecord/index.php
@@ -8,7 +8,7 @@ $currentPage = "Projects";
 <!DOCTYPE html>
 <html lang="en">
 <?php renderHead(
-        "Christian Korndoerfer | Web Developer | For The Record",
+        "Christian Korndoerfer | Web Developer | The Record",
         "During my study in Leipzig and as a part of my masters thesis I started
         working on a new platform for vinyl collectors using Open Data as a basis.",
         "/assets/images/projects/fortherecord/fortherecord1.jpg"
@@ -19,13 +19,13 @@ $currentPage = "Projects";
     <?php require_once(TEMPLATES_PATH . '/navigation.php'); ?>
 
     <div class="container singleProject text">
-        <h1>For The Record</h1>
+        <h1>The Record</h1>
         <div class="row">
             <div class="col-md-10 offset-md-1">
                 <p>
                     During my study in Leipzig and as a part of my <a href="https://www.leipzigschoolofmedia.de/fileadmin/content/Dokumente/Masterarbeiten/CMM/Masterarbeit_Korndoerfer-Christian_2014.pdf" title="Master thesis" target="_blank">masters thesis</a> I started
                     working on a new platform for vinyl collectors using Open Data as a basis. The idea was to replace old school techniques like using excel or worse, bring collectors together, connect and encourage them to rediscover their collections.
-                    By now users only need to provide artist and title of their vinyls in order to add them to their collection. Everything else like tracklist, label, catalog number, genre, etc. will be aggregated automatically with the help of various
+                    By now users only need to provide artist and title of their vinyls in order to add them to their collection. Everything else like track list, label, catalog number, genre, etc. will be aggregated automatically with the help of various
                     music related APIs. After populating the <a href="https://therecord.de/user/1/collection" target="_blank">collection</a>, various sorting and filter tools are provided. In addition a personal collectors profile is created,
                     which provides several <a href="https://therecord.de/user/1" target="_blank">statistics</a> on your collection. Another feature is the <a href="https://therecord.de/user/1/jukebox" target="_blank">Jukebox</a>, which
                     will play random tracks from your collection in an endless loop. Maybe it's time to listen some old records again?
@@ -46,9 +46,9 @@ $currentPage = "Projects";
                     The platform aggregates and visualizes data from various APIs, like Discogs, Spotify, iTunes or Youtube.
                     By combining datasets from different sources throughout the web, new and more valuable data can be created.
                     This well-orchestrated data is then made public as <strong>Open Data</strong> again by using an open API.<br><br>
-                    The whole project is Open Source and realized with the <strong>MVC</strong> based PHP framework <a href="http://laravel.com" title="Laravel" target="_blank">Laravel</a>
+                    The whole project is Open Source and realized with the <strong>MVC</strong> based PHP framework <a href="http://laravel.com" title="Laravel" target="_blank">Laravel</a> in combination with <strong>Vue.js</strong> for some frontend components 
                     and hosted with <a href="http://forge.laravel.com" title="Laravel Forge" target="_blank">Laravel Forge</a> on Digital Ocean.
-                    You can check out the code on <a href="https://github.com/schnubor/fortherecord" title="Fortherecord on Github" target="_blank">Github</a>.
+                    You can check out the code on <a href="https://github.com/schnubor/therecord" title="Fortherecord on Github" target="_blank">Github</a>.
                 </p>
             </div>
         </div>
