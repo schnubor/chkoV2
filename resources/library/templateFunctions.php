7929bb1d3de421a5584e57117002b400cd233d06
schnubor <info@chko.org>
2017-02-06 22:10:36 +0100

    fixed OG meta tags

--- resources/library/templateFunctions.php
+++ resources/library/templateFunctions.php
@@ -16,8 +16,8 @@
 
         echo "\t<title>" . $title . "</title>\n";
         echo "\t<meta name=\"description\" content=\"" . $description . "\">\n";
-        echo "\t<link rel=\"canonical\" href=\"http://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}\">\n";
-        echo "\t<meta property=\"og:url\" content=\"http://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}\">\n";
+        echo "\t<link rel=\"canonical\" href=\"https://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}\">\n";
+        echo "\t<meta property=\"og:url\" content=\"https://{$_SERVER['HTTP_HOST']}{$_SERVER['REQUEST_URI']}\">\n";
         echo "\t<meta property=\"og:type\" content=\"website\">\n";
         echo "\t<meta property=\"og:title\" content=\"" . $title . "\">\n";
         echo "\t<meta property=\"og:image\" content=\"http://{$_SERVER['HTTP_HOST']}" . $ogimage . "\">\n";
