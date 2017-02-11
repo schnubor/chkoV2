b86e2ff7957ba55718fd41f6a10b7f1ccb777a04
schnubor <info@chko.org>
2017-02-11 14:00:48 +0100

    added github link to homepage

--- index.php
+++ index.php
@@ -22,8 +22,9 @@ $useH1 = true;
             </p>
 
             <div class="text" style="margin-top: -40px;">
-                <a class="btn" href="/about"><i class="fa fa-info" aria-hidden="true"></i> About</a>
-                <a class="btn" href="/about/CV.pdf"><i class="fa fa-file-pdf-o" aria-hidden="true"></i> Download CV</a>
+                <a class="btn" href="/about" title="About"><i class="fa fa-info" aria-hidden="true"></i> About</a>
+                <a class="btn" href="https://github.com/schnubor" title="Github" target="_blank"><i class="fa fa-github" aria-hidden="true"></i> Github</a>
+                <a class="btn" href="/about/CV.pdf" title="CV"><i class="fa fa-file-pdf-o" aria-hidden="true"></i> Download CV</a>
             </div>
 
             <?php require_once(TEMPLATES_PATH . '/projects.php'); ?>
